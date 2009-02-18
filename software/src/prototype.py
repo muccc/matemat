35d592a5b90a20ff7c237223108d06472b706fa4
codec <codec@36ddcbd6-1ecb-4bf9-a6ab-f6b9f52bc24d>
2009-02-18 21:19:34 +0000

    Stupid prototype to implement basic functionality. Run script, push button, enjoy mate.

---
+++ software/src/prototype.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+#
+# ----------------------------------------------------------------------------
+# "THE MATE-WARE LICENSE"
+# codec <codec@muc.ccc.de> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a mate in return.
+# ----------------------------------------------------------------------------
+#
+
+import serial
+import time
+import sys
+
+def lcdmsg(val):
+        lcdmsg = "D                   \n"
+        serial.write(lcdmsg)
+        time.sleep(0.1)
+        lcdmsg = "D%s\n" % val
+        print lcdmsg
+        serial.write(lcdmsg)
+
+serdev = "/dev/ttyS0"
+serial = serial.Serial(serdev, 115200)
+
+lcdmsg("foo")
+
+purse = open("/mnt/token/purse", "r")
+pursedata = purse.readlines()
+
+tokencount = 0
+for line in pursedata:
+        tokencount = tokencount+1
+
+lcdmsg("Token Count: %s" % tokencount)
+
+while 1:
+        time.sleep(0.1)
+        serial.write("V")
+        mmstate = serial.read(1)
+        print mmstate
+        if mmstate != "-":
+                serial.write("O")
+                mmstate = serial.read(1)
+                print mmstate
+                if mmstate == "D":
+                        lcdmsg("Okay")
+                        sys.exit(0)
+# EOF
