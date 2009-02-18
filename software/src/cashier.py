f5635983395978f3ce2a3923cd6c6006d05f383c
codec <codec@36ddcbd6-1ecb-4bf9-a6ab-f6b9f52bc24d>
2009-02-18 22:28:56 +0000

    Just rewriting code from prototype.

--- software/src/cashier.py
+++ software/src/cashier.py
@@ -9,4 +9,36 @@
 # ----------------------------------------------------------------------------
 #
 
+import serial
+import time
+import sys
+
+
+###
+## msg2lcd(message): Send a message to the matemat LCD.
+## TODO: Handle 'D' returned from controller.
 #
+def msg2lcd(val):
+	lcdmsg = "D%s\n" % val
+	serial.write(lcdmsg)
+	debug && print "LCD-Message: %s" % val
+
+def readPurse():
+	purse = open("/mnt/token/purse", "r")
+	pursedata = purse.readlines()
+
+	tokencount = 0
+	for token in pursedata:
+		tokencount = tokencount+1
+
+	debug && print "Token Count: %s" % tokencount
+	return tokencount
+
+def talkSerial(send, expect):
+	serial.write(send)
+	reply = serial.read(1)
+	debug && print "talkSerial() reply: %s" % reply
+	if reply == expect:
+		return TRUE
+	else
+		return FALSE
