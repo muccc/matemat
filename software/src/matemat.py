2a9f6af2077e7f20d85726521d159e0f128d569c
schneider <schneider@36ddcbd6-1ecb-4bf9-a6ab-f6b9f52bc24d>
2009-02-20 11:15:22 +0000

    New matemat module which encapsulates the device

---
+++ software/src/matemat.py
@@ -0,0 +1,49 @@
+import serial
+import serialinterface
+import time
+
+class Matemat:
+    def __init__(self):
+        self.interface = serialinterface.SerialInterface("/dev/ttyS0",115200,5);
+
+    def waitForReply(self,reply):
+        while True:
+            msg = self.interface.readMessage()
+            if msg == False:
+                return False
+            if msg in reply:
+                return msg
+
+
+    def writeLCD(self, msg):
+        msg = "d"+msg
+        self.interface.writeMessage(msg);
+        return self.waitForReply(["dD"])
+    
+    def getPriceline(self):
+        self.interface.writeMessage("p")
+        while True:
+            msg = self.interface.readMessage()
+            if msg == False:
+                return False
+            if msg[0] == 'p':
+                return int(msg[2])
+    def serve(self,priceline):
+        self.interface.writeMessage("s"+str(priceline))
+        ret = self.waitForReply(["sO","sN"])
+        if ret == False:
+            return False
+        if ret == "sN":
+            return False
+    def completeserve(self):
+        return self.waitForReply(["sD"])
+
+m = Matemat()
+m.writeLCD("luvv")
+while m.getPriceline() != 3:
+    time.sleep(0.2)
+m.serve(3)
+m.completeserve()
+
+
+
