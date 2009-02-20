9c72dfe9bb3d16e7a9734f21c403a19a038acfaf
codec <codec@36ddcbd6-1ecb-4bf9-a6ab-f6b9f52bc24d>
2009-02-20 20:36:48 +0000

    Moved

---
+++ software/src/ng/lib/matemat.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import serial
+import serialinterface
+import time
+
+class Matemat:
+    def __init__(self):
+        self.interface = serialinterface.SerialInterface("/dev/ttyS0",115200,5);
+
+    def waitForReply(self,reply):
+        while True:
+            msg = self.interface.readMessage()
+            if msg == False:
+                return False
+            if msg in reply:
+                return msg
+
+
+    def writeLCD(self, msg):
+        msg = "d"+msg
+        self.interface.writeMessage(msg);
+        return self.waitForReply(["dD"])
+    
+    def getPriceline(self):
+        self.interface.writeMessage("p")
+        while True:
+            msg = self.interface.readMessage()
+            if msg == False:
+                return -1
+            if msg[0] == 'p':
+                return int(msg[2])
+    def serve(self,priceline):
+        self.interface.writeMessage("s"+str(priceline))
+        ret = self.waitForReply(["sO","sN"])
+        if ret == False:
+            return False
+        if ret == "sN":
+            return False
+        return True
+    def completeserve(self):
+        return self.waitForReply(["sD"])
+
+# "Testing"
+
+if __name__ == '__main__':
+    m = Matemat()
+    m.writeLCD("luvv")
+    while m.getPriceline() != 3:
+        time.sleep(0.2)
+    m.serve(3)
+    m.completeserve()
