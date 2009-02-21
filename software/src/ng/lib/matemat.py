81c9d2d64d97fdcbdeb512ab2241d30f7c439f72
codec <codec@36ddcbd6-1ecb-4bf9-a6ab-f6b9f52bc24d>
2009-02-21 22:25:09 +0000

    Modified Matemat() to use Logger

--- software/src/ng/lib/matemat.py
+++ software/src/ng/lib/matemat.py
@@ -3,34 +3,47 @@
 import serial
 import serialinterface
 import time
+import logger
 
 class Matemat:
     def __init__(self):
         self.interface = serialinterface.SerialInterface("/dev/ttyS0",115200,5);
+        self.log = logger.Logger('Matemat')
+        self.log.debug('__init__(): invoked')
 
     def waitForReply(self,reply):
+        self.log.debug('waitForReply(): invoked')
+        self.log.info('waitForReply(): reply=%s' % reply)
         while True:
             msg = self.interface.readMessage()
             if msg == False:
                 return False
             if msg in reply:
+                self.log.info('waitForReply(): msg=%s' % msg)
                 return msg
 
 
     def writeLCD(self, msg):
+        self.log.debug('writeLCD(): invoked')
+        self.log.info('writeLCD(): msg=%s' % msg)
         msg = "d"+msg
         self.interface.writeMessage(msg);
         return self.waitForReply(["dD"])
     
     def getPriceline(self):
+        self.log.debug('getPriceline(): invoked')
         self.interface.writeMessage("p")
         while True:
             msg = self.interface.readMessage()
             if msg == False:
                 return -1
             if msg[0] == 'p':
+                self.log.info('getPriceline(): priceline=%s' % msg[2])
                 return int(msg[2])
+
     def serve(self,priceline):
+        self.log.debug('serve(): invoked')
+        self.log.info('serve(): priceline=%s' % priceline)
         self.interface.writeMessage("s"+str(priceline))
         ret = self.waitForReply(["sO","sN"])
         if ret == False:
@@ -38,7 +51,9 @@ class Matemat:
         if ret == "sN":
             return False
         return True
+
     def completeserve(self):
+        self.log.debug('completeserve(): invoked')
         return self.waitForReply(["sD"])
 
 # "Testing"
